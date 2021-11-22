a9eb72f16902ff8b6f5924b579e7019a6b22b87e
Minhee Cho <aezjk12@ewhain.net>
2021-11-22 16:12:36 +0900

    Delete BigDataApplication directory

--- BigDataApplication/footer.php
+++
@@ -1,9 +0,0 @@
-        <div id="footer_content">
-            <p id="footer_logo">Big Data Application | <span> Quadruple Team</span></p>
-            
-            <ul id="author">
-                <li>Contributor</li>
-                <li>이정하 이소림</li>
-                <li>이현주 조민희</li>
-            </ul>
-        </div>
